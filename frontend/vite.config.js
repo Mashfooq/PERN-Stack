95e643b31f223513fc45f07eee9171b64babd1d9
Mashfooq <mashhussmashfoo@gmail.com>
2024-01-29 20:35:20 +0530

    Updated vite.cinfig.js "Added port and server"

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // we need to define which port we will run on
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true, // Here
+    strictPort: true,
+    port: 5173,
+  }
 })
 
 
